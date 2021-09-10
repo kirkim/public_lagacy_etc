b50c8f6933163b90aef60b0a8ae9d6c11306a805
kimkirim <kirimrlfla@naver.com>
2021-09-10 20:19:40 +0900

    chapter4

--- NodeJS/src/server.js
+++ NodeJS/src/server.js
@@ -1,29 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
 
 const PORT = 4000;
 const app = express();
-
-const logger = morgan("dev");  // morgan에서 index.js파일 보면 next()를 호출함을 볼 수 있음
-//const logger = morgan("combine");
-//const logger = morgan("short");
-//const logger = morgan("tiny");
-
-const handleHome = (request, response) => {
-	return (response.send("I love middlewares"));
-};
-
-const handleProtected = (req, res) => {
-	return (res.send("Welcome to the private lounge."));
-}
-
-const handleLogin = (req, res) => {
-	return (res.send({ message: "Login here." }));
-}
-
+const logger = morgan("dev");
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
 
-const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT} 🌻`);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () => {
+	console.log(`Server listenting on port http://localhost:${PORT} 🌻`);
+}
 app.listen(PORT, handleListening)
