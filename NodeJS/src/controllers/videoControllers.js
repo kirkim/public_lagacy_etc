3dd3718e0406630ac8378ed468a43cf9399d6566
kimkirim <kirimrlfla@naver.com>
2021-09-12 00:06:03 +0900

    done

--- NodeJS/src/controllers/videoControllers.js
+++ NodeJS/src/controllers/videoControllers.js
@@ -1,6 +1,10 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("See");
+export const see = (req, res) => {
+	return res.send(`Watch Video #${req.params.id}`);
+}
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delte Video");
+export const deleteVideo = (req, res) => {
+	return res.send("Delete Video");
+}
--- NodeJS/src/routers/videoRouter.js
+++ NodeJS/src/routers/videoRouter.js
@@ -3,9 +3,8 @@ import { see, edit, upload, deleteVideo } from '../controllers/videoControllers'
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo)
-
+videoRouter.get("/:id(\\d+)", see);  // 이런식으로 숫자만 받도록하면 upload를 아래순번에 보내도됨
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo)
+videoRouter.get("/upload", upload);  // 순서중요 :id보다 아래있음 오류생김
 export default videoRouter;
