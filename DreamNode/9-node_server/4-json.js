2961b2f1e983fcca05ea4645aa10da680fbcdf5e
kimkirim <kirimrlfla@naver.com>
2021-09-26 15:51:45 +0900

    Sdf

--- DreamNode/9-node_server/4-json.js
+++ DreamNode/9-node_server/4-json.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
   const method = req.method; // how?, action?
   if (url === "/courses") {
     if (method === "GET") {
-      res.writeHead(200, { "Contetn-Type": "application/json" });
+      res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(courses));
     } else if (method === "POST") {
       const body = [];
