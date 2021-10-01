532776f2045d2567ee0c086e56eb9a3e2d6972fb
kimkirim <kirimrlfla@naver.com>
2021-10-01 10:36:45 +0900

    cookie

--- NodeJS/src/server.js
+++ NodeJS/src/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import globalRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import session from "express-session";
 
 const app = express();
 const logger = morgan("dev");
@@ -11,6 +12,27 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views"); // view의 위치를 서버폴더에서 찾기때문 이렇게 지정해줘야함
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); // form을 이해하고 js형태로 바꿔줌
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+  });
+  next();
+});
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
