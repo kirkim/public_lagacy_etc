c3d299c9cafd8b92b62c5294c12388ad7ef51581
kimkirim <kirimrlfla@naver.com>
2021-10-18 12:07:56 +0900

    thumbnail

--- NodeJS/src/routers/videoRouter.js
+++ NodeJS/src/routers/videoRouter.js
@@ -25,6 +25,12 @@ videoRouter
   .route("/upload")
   .get(getUpload)
   .all(protectMiddleWare)
-  .post(videoUpload.single("video"), postUpload);
+  .post(
+    videoUpload.fields([
+      { name: "video", maxCount: 1 },
+      { name: "thumb", maxCount: 1 },
+    ]),
+    postUpload
+  );
 
 export default videoRouter;
