cb90d0e8e1ba146c7f6ec093f0b36ff6036a614e
kimkirim <kirimrlfla@naver.com>
2021-11-14 21:58:40 +0900

    s

--- src/client/components/basicBoard.ts
+++
@@ -1,8 +0,0 @@
-import { BoardList } from './board/boardList.js';
-import { BaseComponent } from './components.js';
-
-export class BasicBoard extends BaseComponent<HTMLElement> {
-  constructor(list: BoardList) {
-    super(`${list.render()}`);
-  }
-}
--- src/client/components/board/boardList.ts
+++ src/client/components/board/boardList.ts
@@ -6,15 +6,18 @@ export class BoardList {
     this.render();
   }
 
+  onWatchPostListener(event: Event) {
+    const target = event.target as HTMLElement;
+    if (target.dataset.id) {
+      const detail = new PostDetail(target.dataset.id);
+      return detail.render();
+    }
+    return '';
+  }
   render() {
     let render = '';
     this.data.forEach((post) => {
-      const title = `<div class="board-title"><span id-data="${post.id}">${post.title}</span></div>`;
-      const titleLink = document.querySelector('.board-title span')! as HTMLSpanElement;
-      titleLink.addEventListener('click', () => {
-        const detail = new PostDetail(post);
-        console.log(detail);
-      });
+      const title = `<div class="board-title"><span data-id="${post.id}">${post.title}</span></div>`;
       render = render + `<li>${title}</li>`;
     });
     return `<ul>${render}</ul>`;
