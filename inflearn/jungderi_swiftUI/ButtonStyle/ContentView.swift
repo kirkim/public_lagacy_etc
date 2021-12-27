33d3b5e8c67a1a5607a9aefa7727c444305201e1
kimkirim <kirimrlfla@naver.com>
2021-12-27 19:58:57 +0900

    strudy custom button

--- inflearn/jungderi_swiftUI/ButtonStyle/ContentView.swift
+++ inflearn/jungderi_swiftUI/ButtonStyle/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: {
+                print("탭")
+            }, label: {
+                Text("탭")
+            }).buttonStyle(MyButtonStyle(color: .blue, type: .tap))
+            
+            Button(action: {
+                print("롱탭")
+            }, label: {
+                Text("롱탭")
+            }).buttonStyle(MyButtonStyle(color: .green, type: .long))
+            
+            Button(action: {
+                print("축소탭")
+            }, label: {
+                Text("축소탭")
+            }).buttonStyle(MyButtonStyle(color: .yellow, type: .small))
+
+            Button(action: {
+                print("회전탭")
+            }, label: {
+                Text("회전탭")
+            }).buttonStyle(MyButtonStyle(color: .red, type: .rotation))
+            
+            Button(action: {
+                print("블러탭")
+            }, label: {
+                Text("블러탭")
+            }).buttonStyle(MyButtonStyle(color: .black, type: .blur))
+            
+            Button(action: {
+                print("샘플버튼")
+            }, label: {
+                Text("샘플버튼")
+            }).buttonStyle(SampleButton())
+        }
     }
 }
 
