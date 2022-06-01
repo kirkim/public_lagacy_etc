fb4a04939d060487206c9318abafca3c8092238a
kimkirim <kirimrlfla@naver.com>
2022-06-01 20:14:54 +0900

    test

--- backend/build/config.js
+++ backend/build/config.js
@@ -3,6 +3,7 @@ import path from 'path';
 dotenv.config();
 function required(key, defaultValue) {
     const value = process.env[key] || defaultValue;
+    console.log(value, process.env.PORT);
     if (value == null) {
         throw new Error(`${key} is undefined`);
     }
@@ -19,7 +20,7 @@ const config = {
         url: required('BASE_URL', path.resolve() + '/assets'),
     },
     server: {
-        baseUrl: required('SERVER_BASE_URL', process.env.PORT),
+        baseUrl: required('PORT', 4000),
     },
 };
 export default config;
